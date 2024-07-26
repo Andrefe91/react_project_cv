d480bec68af62ad53b7eb1c1998c4c7a3ba6ef5d
Andres Muñoz <andrefe91@gmail.com>
2024-07-26 17:50:33 -0500

    Prettier pass to some files

--- my_curriculum_vitae/src/components/App.jsx
+++ my_curriculum_vitae/src/components/App.jsx
@@ -5,68 +5,70 @@ import Form from "./Form.jsx";
 import Result from "./Result.jsx";
 
 function App() {
-  let personalInfo = {
-    'firstName' : "",
-    'lastName' : "",
-    'title' : "",
-    'email' : "",
-    'phone' : "",
-    'gender' : "Male",
-    'nationality' : "United States",
-  };
+	let personalInfo = {
+		firstName: "",
+		lastName: "",
+		title: "",
+		email: "",
+		phone: "",
+		gender: "Male",
+		nationality: "United States",
+	};
 
-  let educationalInfo = [{}];
+	let educationalInfo = [{}];
 
-  let experienceInfo = [{}, {}];
+	let experienceInfo = [{}, {}];
 
-
-  const [isFormMode, setIsFormMode] = useState(true);
+	const [isFormMode, setIsFormMode] = useState(true);
 	const [userData, setUserData] = useState({
-    'personalInfo' : personalInfo,
-    'educationalInfo' : educationalInfo,
-    'experienceInfo' : experienceInfo,
-  });
+		personalInfo: personalInfo,
+		educationalInfo: educationalInfo,
+		experienceInfo: experienceInfo,
+	});
 
-  function userDataManipulation(e) {
-    const [sectionName, propertyName, positionId] = e.target.name.split('.');
+	function userDataManipulation(e) {
+		const [sectionName, propertyName, positionId] = e.target.name.split(".");
 
-    if (sectionName == "personalInfo") {
-      setUserData({
-        ...userData,
-        [sectionName] : {
-          ...userData[sectionName],
-          [propertyName] : e.target.value
-        }
-      })
-    } else {
-      setUserData({
-        ...userData,
-        [sectionName] :  {
-          ...userData[sectionName],
-          [positionId] : { //Returns the array in the positionId
-           ...userData[sectionName][positionId],
-            [propertyName] : e.target.value
-          }
-        }
-      })
-    }
-  }
+		if (sectionName == "personalInfo") {
+			setUserData({
+				...userData,
+				[sectionName]: {
+					...userData[sectionName],
+					[propertyName]: e.target.value,
+				},
+			});
+		} else {
+			setUserData({
+				...userData,
+				[sectionName]: {
+					...userData[sectionName],
+					[positionId]: {
+						//Returns the array in the positionId
+						...userData[sectionName][positionId],
+						[propertyName]: e.target.value,
+					},
+				},
+			});
+		}
+	}
 
+	function addExperience() {
+		// [Todo]
+	}
 
-
-  function addExperience() {
-    // [Todo]
-  }
-
-  function addEducation() {
-    // [Todo]
-  }
+	function addEducation() {
+		// [Todo]
+	}
 
 	return (
 		<>
 			<p>Work in progress...</p>
 
-			{isFormMode ? <Form userData={userData} onChange={userDataManipulation}/> : <Result userData={userData}/>}
+			{isFormMode ? (
+				<Form userData={userData} onChange={userDataManipulation} />
+			) : (
+				<Result userData={userData} />
+			)}
 
 			<button onClick={() => setIsFormMode(!isFormMode)}>App Mode</button>
 		</>
--- my_curriculum_vitae/src/components/Form.jsx
+++ my_curriculum_vitae/src/components/Form.jsx
@@ -1,24 +1,22 @@
-import Section from '../components/Section.jsx'
-import JobExperience from './JobExperience.jsx'
+import Section from "../components/Section.jsx";
+import JobExperience from "./JobExperience.jsx";
 
-import '../styles/Form.css'
-import PersonalInfo from './PersonalInfo.jsx'
+import "../styles/Form.css";
+import PersonalInfo from "./PersonalInfo.jsx";
 
-export default function Form({userData, onChange}){
+export default function Form({ userData, onChange }) {
+	return (
+		<>
+			<h3>Form Component</h3>
 
+			<Section> {/* Component */}
+				<PersonalInfo userData={userData} onChange={onChange} />
+			</Section>
 
-    return (
-        <>
-            <h3>Form Component</h3>
-
-            <Section> {/* Component */}
-                <PersonalInfo userData={userData} onChange={onChange}/>
-            </Section>
-
-            <Section>
-                <JobExperience userData={userData} onChange={onChange} positionId={0}/>
-                <JobExperience userData={userData} onChange={onChange} positionId={1}/>
-            </Section>
-        </>
-    )
+			<Section>
+				<JobExperience userData={userData} onChange={onChange} positionId={0} />
+				<JobExperience userData={userData} onChange={onChange} positionId={1} />
+			</Section>
+		</>
+	);
 }
