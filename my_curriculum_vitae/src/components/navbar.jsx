76e6047bfd4a1e4168dfc58fe596f6bab0fa10b5
Andres Munoz <andrefe91@gmail.com>
2024-07-29 23:11:58 -0500

    Navbar to app

---
+++ my_curriculum_vitae/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import '../styles/navbar.css'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <p>
+                [  Your Curriculum, this time, more vitae  ]
+            </p>
+        </nav>
+    )
+}
